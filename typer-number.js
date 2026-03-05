8d15f289f83fa0407d3542b6e98259addb2ccee4
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-03-05 15:19:34 +0000

    salvar

--- typer-number.js
+++ typer-number.js
@@ -1,7 +1,17 @@
-// tipo number
+// tipo number 
 const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero + segundoNumero;
 
 console.log(operacaoMatematica);
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const PontoFlutuanteSemZero =.5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log (novaOperacao);
+
